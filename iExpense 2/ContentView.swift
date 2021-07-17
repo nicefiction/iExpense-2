12645ab836277742ac713d19102e73cf4c623be7
Olivier Van hamme <nicefiction@gmail.com>
2021-07-17 10:43:15 +0200

    Initial Commit

---
+++ iExpense 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense 2
+//
+//  Created by Olivier Van hamme on 17/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense 2/iExpense_2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_2App.swift
+//  iExpense 2
+//
+//  Created by Olivier Van hamme on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
