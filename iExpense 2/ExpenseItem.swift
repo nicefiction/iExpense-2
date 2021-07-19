b9b54834611bdba814d46840a2a4795227916f82
Olivier Van hamme <nicefiction@gmail.com>
2021-07-19 11:18:09 +0200

    Adds explanatory comments to the Expenses class .

--- iExpense 2/ExpenseItem.swift
+++ iExpense 2/ExpenseItem.swift
@@ -7,7 +7,7 @@ import Foundation
 struct ExpenseItem: Identifiable,
                     Codable {
    
-   let id: UUID = UUID()
+   var id: UUID = UUID()
    var name: String
    var type: String
    var amount: Int
--- iExpense 2/Expenses.swift
+++ iExpense 2/Expenses.swift
@@ -16,13 +16,25 @@ class Expenses: ObservableObject {
                                       forKey: "Items")
          }
       }
+      /**
+       We use a `didSet` property observer on the `items` property of `Expenses`,
+       so that whenever an item gets added or removed
+       we will write out changes .
+       */
    }
    
    
    
    // MARK: - INITIALIZER METHODS:
+   // SOURCE: https://www.hackingwithswift.com/books/ios-swiftui/making-changes-permanent-with-userdefaults
    
-   init() {
+   /**
+    We use a custom initializer for the `Expenses` class ,
+    so that when we make an instance of it
+    we load any saved `data` from `UserDefaults` .
+    */
+   init()
+   throws {
       // 1. Reads whatever is in “Items” as a Data object :
       if
          let _items = UserDefaults.standard.data(forKey: "Items") {
@@ -41,3 +53,42 @@ class Expenses: ObservableObject {
       }
    }
 }
+
+/*
+ struct Bread: Codable {
+     
+     let id: Int
+     let name: String
+     let bakery: String
+     
+     // STEP 1 of 3 , CodingKeys :
+     enum CodingKeys: String ,
+                      CodingKey {
+         case id
+         case name
+         case bakery
+     }
+     
+     // STEP 2 of 3 , init(decoder) :
+     init(from decoder: Decoder)
+     throws {
+         
+         let jsonContainer = try decoder.container(keyedBy : CodingKeys.self)
+         
+         self.id     = try jsonContainer.decode(Int.self ,    forKey : CodingKeys.id)
+         self.name   = try jsonContainer.decode(String.self , forKey : CodingKeys.name)
+         self.bakery = try jsonContainer.decode(String.self , forKey : CodingKeys.bakery)
+     }
+     
+     // STEP 3 of 3 , encode(to) :
+     func encode(to encoder: Encoder)
+     throws {
+         
+         var jsonContainer = encoder.container(keyedBy : CodingKeys.self)
+         
+         try jsonContainer.encode(id ,     forKey : CodingKeys.id)
+         try jsonContainer.encode(name ,   forKey : CodingKeys.name)
+         try jsonContainer.encode(bakery , forKey : CodingKeys.bakery)
+     }
+ }
+ */
